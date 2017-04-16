f17bcb6d5d0df0f993a04708670de276e26351c5
Cody Higgins <cody.higginsdev18@gmail.com>
2017-04-16 14:10:51 -0600

    created mock data for documents

--- freelance-camp-frtend/app/documents/documents.component.js
+++ freelance-camp-frtend/app/documents/documents.component.js
@@ -8,6 +8,31 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@angular/core");
 let DocumentsComponent = class DocumentsComponent {
+    constructor() {
+        this.documents = [
+            {
+                title: "First Doc",
+                description: "Money to be made",
+                file_url: 'http://google.com',
+                updated_at: '4/16/2017',
+                image_url: 'http://google.com'
+            },
+            {
+                title: "Second Doc",
+                description: "Money to be made",
+                file_url: 'http://google.com',
+                updated_at: '4/16/2017',
+                image_url: 'http://google.com'
+            },
+            {
+                title: "Third Doc",
+                description: "Money to be made",
+                file_url: 'http://google.com',
+                updated_at: '4/16/2017',
+                image_url: 'http://google.com'
+            }
+        ];
+    }
 };
 DocumentsComponent = __decorate([
     core_1.Component({
--- freelance-camp-frtend/app/documents/documents.component.ts
+++ freelance-camp-frtend/app/documents/documents.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Document } from './document';
 
 @Component({
 	moduleId: module.id,
@@ -7,5 +8,27 @@ import { Component } from '@angular/core';
 })
 
 export class DocumentsComponent {
-	
+	documents: Document[] = [
+		{ 
+			title: "First Doc",
+			description: "Money to be made",
+			file_url: 'http://google.com',
+			updated_at: '4/16/2017',
+			image_url: 'http://google.com'
+		}
+		{ 
+			title: "Second Doc",
+			description: "Money to be made",
+			file_url: 'http://google.com',
+			updated_at: '4/16/2017',
+			image_url: 'http://google.com'
+		}
+		{ 
+			title: "Third Doc",
+			description: "Money to be made",
+			file_url: 'http://google.com',
+			updated_at: '4/16/2017',
+			image_url: 'http://google.com'
+		}
+	]
 }
