b43b144a55877cdfd87a08627b32d72ccaaa8b49
Cody Higgins <cody.higginsdev18@gmail.com>
2017-04-21 09:10:45 -0600

    added nav styles, doc styles and new proposals styles.

--- freelance-camp-frtend/app/documents/documents.component.ts
+++ freelance-camp-frtend/app/documents/documents.component.ts
@@ -4,7 +4,8 @@ import { Document } from './document';
 @Component({
 	moduleId: module.id,
 	selector: 'documents',
-	templateUrl: 'documents.component.html'
+	templateUrl: 'documents.component.html',
+	styleUrls: ['documents.component.css']
 })
 
 export class DocumentsComponent {
@@ -15,21 +16,21 @@ export class DocumentsComponent {
 			description: "Money to be made",
 			file_url: 'http://google.com',
 			updated_at: '4/16/2017',
-			image_url: 'http://google.com'
+			image_url: 'http://youngtopublishing.com/wp-content/uploads/2017/02/Freelancer-Fair-couple.jpg'
 		},
 		{ 
 			title: "Second Doc",
 			description: "Money to be made",
 			file_url: 'http://google.com',
 			updated_at: '4/16/2017',
-			image_url: 'http://google.com'
+			image_url: 'http://youngtopublishing.com/wp-content/uploads/2017/02/Freelancer-Fair-couple.jpg'
 		},
 		{ 
 			title: "Third Doc",
 			description: "Money to be made",
 			file_url: 'http://google.com',
 			updated_at: '4/16/2017',
-			image_url: 'http://google.com'
+			image_url: 'http://youngtopublishing.com/wp-content/uploads/2017/02/Freelancer-Fair-couple.jpg'
 		}
 	]
 }
